2ac8454c71ec0ea20bd6a399fc8496f1c0eb9ab6
testsasitest <sasi2103@bugcrowdninja.com>
2017-08-28 22:47:50 +0300

    @sasi2103 add output restult to a file.

--- scan.rb
+++ scan.rb
@@ -11,6 +11,21 @@ def get_option(key)
   selected.split('=').last
 end
 
+def write_results(output) 
+  begin
+    puts " Start writing final results"
+    file = File.open("output", "ab+")
+    file.write(output) 
+    puts " Finish writing final results"
+  rescue IOError => e
+    #some error occur, dir not writable etc.
+    puts e;
+  ensure
+    file.close unless file.nil?
+  end
+end 
+
+
 ip_address = get_option('ip')
 host = get_option('host')
 ignore_http_codes = get_option('ignore-http-codes') || '404'
@@ -18,6 +33,7 @@ ignore_content_length = get_option('ignore-content-length') || 0
 wordlist_file = get_option('wordlist') || 'wordlist'
 ssl = get_option('ssl') || 'off'
 port = get_option('port') || (ssl == 'on' ? 443 : 80)
+output = get_option('output') || 'output.txt'
 
 if ip_address.nil? || host.nil?
   puts 'Usage: ruby scan.rb --ip=<ip-address> --host=<host>'
@@ -30,13 +46,14 @@ if ip_address.nil? || host.nil?
   puts '    --ignore-content-length=<value>'
   puts '    --wordlist=<file location>'
   puts '    --ssl=<on|off>'
+  puts '    --output=<output - default current directory, output.txt>'
   exit
 end
 
 port = port.to_i
 ignore_http_codes = ignore_http_codes.split(',').map { |code| code.to_i }
 ignore_content_length = ignore_content_length.to_i
-
+result = StringIO.new
 IO.read(wordlist_file).split("\n").each do |virtual_host|
   hostname = virtual_host.gsub('%s', host)
   
@@ -50,12 +67,13 @@ IO.read(wordlist_file).split("\n").each do |virtual_host|
     next if ignore_http_codes.include?(response.code.to_i)
     next if ignore_content_length > 0 && ignore_content_length == response['content-length'].to_i
 
-    puts "Found: #{hostname} (#{response.code})"
+    result << "Found: #{hostname} (#{response.code})\n"
     response.to_hash.each do |header, values|
-      puts "  #{header}:"
+      result << "  #{header}:"
       values.each do |value|
-        puts "    #{value}"
+        result<< "\n    #{value}\n"
       end
     end
   end
 end
+write_results(result.string)
